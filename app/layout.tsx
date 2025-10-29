99d7e8f8ff2784d841e4cf0e3ed17b46a9117f42
MDR <imdanya946@gmail.com>
2025-10-29 18:01:49 +0200

    add google script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -62,6 +63,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.variable}`}>
         {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17687925953"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17687925953');
+          `}
+        </Script>
+
       </body>
     </html>
   );
